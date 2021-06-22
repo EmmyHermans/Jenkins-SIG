99e7bada0536def27eb015ec97f591b5b12e3f95
Emmy Hermans <emmy.hermans@amis.nl>
2021-06-22 16:29:21 +0200

    a change

--- koa-angular-app/angular-frontend/src/app/app.component.spec.ts
+++ koa-angular-app/angular-frontend/src/app/app.component.spec.ts
@@ -18,7 +18,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-frontend'`, () => {
+  it(`should have as title 'angular-frontend' `, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-frontend');
