1d4a8e438e21aef71880215fbd7b52d9d7630d2f
Emmy Hermans <emmy.hermans@amis.nl>
2021-07-01 20:51:49 +0200

    kopieren frontend

--- koa-angular-app/koa-backend/src/app.js
+++ koa-angular-app/koa-backend/src/app.js
@@ -2,11 +2,22 @@ const Koa = require("koa");
 const router = require("./routes");
 const bodyParser = require("koa-bodyparser");
 const cors = require("koa-cors");
+const serve = require("koa-static");
 
 const app = new Koa();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(serve(__dirname + "/public"));
+}
 
 app.use(bodyParser());
 app.use(cors());
 app.use(router.routes());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(function* index() {
+    yield send(this, __dirname + "/public/index.html");
+  });
+}
 
 module.exports = app;
